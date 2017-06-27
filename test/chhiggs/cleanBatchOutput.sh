afcdf03cb551b31d6a414a2df1cc3a38c82e663f
Hugues <hbrun@cern.ch>
2017-06-27 13:57:41 +0200

    remove the useless code for limits

--- test/chhiggs/cleanBatchOutput.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f *.sh.e*
-rm -f *.sh.o*
-rm -r LSFJOB_*
--- test/chhiggs/lumi.sh
+++
@@ -1,3 +0,0 @@
-cp test/chhiggs/results_ttbar/*SingleMuon*.json > myjson.json
-<edit myjson.json to remove the additional "}{"
-python scripts/lumi/lcr2/lcr2.py -i myjson.json
